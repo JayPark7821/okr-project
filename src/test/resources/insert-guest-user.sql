1eb48ffe6749d69306526f2c3bcf037d18a54c1d
jaypark7821 <pjhyun1990@gmail.com>
2022-12-13 23:48:02 +0900

    test: integration tests for UserFacade

---
+++ src/test/resources/insert-guest-user.sql
@@ -0,0 +1,4 @@
+INSERT INTO guest (guset_uuid, guest_id, guest_name, email, provider_type, profile_image_url)
+VALUES ('guest-uuid', 'test-id', 'test-name', 'test@email.com', 'LOCAL', '');
+
+delete from user_table ;
